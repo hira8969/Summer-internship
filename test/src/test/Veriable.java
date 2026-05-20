012212a5681110cb20332a98559467cdae64ce0a
hira8969 <hk2493200@gmail.com>
2026-05-20 17:34:26 +0530

    task-1-complete
    task-1-complete

--- test/src/test/Veriable.java
+++ test/src/test/Veriable.java
@@ -6,6 +6,16 @@ public class Veriable {
 		// veriable intro 
 		int a1=10; 
 		int b=10;
-		
+		String name="Hiralal Kumar";
+		int reg= 123;
+		long  no=7970836127L;
+		char sec= 'B';
+		boolean isstudent=true;
+		System.out.println(a1+b);
+		System.out.println("Name is: "+ name);
+		System.out.println("Reg No:- "+ reg);
+		System.out.println("Double value: "+ no);
+		System.out.println("Sec is:- "+ sec);
+		System.out.println("Student :- "+ isstudent);
 	}
 }
---
+++ test/src/test/allprimitivedatatypes.java
@@ -0,0 +1,36 @@
+package test;
+
+public class allprimitivedatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // this is for Integer 
+        byte byteV = 127;
+        short shortV = 32000;
+        int intV = 100000;
+        long longV = 123456789L;
+
+        // this is for Decimal 
+        float floatV = 12.34f;
+        double doubleV = 123.456789;
+
+        // this  is for Char
+        char charV = 'A';
+
+        // this is for Bool 
+        boolean boolv = true;
+
+        System.out.println("Byte Value: " + byteV);
+        System.out.println("Short Value: " + shortV);
+        System.out.println("Int Value: " + intV);
+        System.out.println("Long Value: " + longV);
+
+        System.out.println("Float Value: " + floatV);
+        System.out.println("Double Value: " + doubleV);
+
+        System.out.println("Char Value: " + charV);
+
+        System.out.println("Boolean Value: " + boolv);
+	}
+
+}

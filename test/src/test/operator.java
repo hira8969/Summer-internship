32c929e3fae8378b9751e295f918a452f3d0979a
hira8969 <hk2493200@gmail.com>
2026-05-21 14:29:20 +0530

    operator
    Arithmetic Op

---
+++ test/src/test/operator.java
@@ -0,0 +1,19 @@
+package test;
+
+public class operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println("Arithmetic Operator");
+		System.out.println("Sum :- "+a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+
+	}
+
+}

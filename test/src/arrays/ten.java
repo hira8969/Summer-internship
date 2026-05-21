95ac6d628c11c51dcf099814915cbfed0255c91a
hira8969 <hk2493200@gmail.com>
2026-05-21 21:39:29 +0530

    solve
    solve 11

---
+++ test/src/arrays/ten.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class ten {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {1,2,3,4,5,6};
+		int n= arr.length;
+		
+		
+			System.out.println("2nd Last Elements :-  "+  arr[n-2]);
+		
+	
+	}
+
+}

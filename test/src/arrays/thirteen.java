abeb85034b735b974926d85976febddbf4bb21f7
hira8969 <hk2493200@gmail.com>
2026-05-24 22:29:01 +0530

    update
    question -13 solve

--- test/src/arrays/thirteen.java
+++ test/src/arrays/thirteen.java
@@ -4,7 +4,22 @@ public class thirteen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = {1, 0, 5, 0, 7, 0, 9, 2, 0};
+		countZero(arr);
+        
+	}
+	public static void countZero(int arr[])
+	{
+		int count = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of zeros in array: " + count);
 	}
 
 }

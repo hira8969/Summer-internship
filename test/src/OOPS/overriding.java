a24cdbf851e3391c24d730fbc281ff1dce5ab7a9
hira8969 <hk2493200@gmail.com>
2026-05-26 14:41:59 +0530

    method-overloading
    methodoverloading

---
+++ test/src/OOPS/overriding.java
@@ -0,0 +1,27 @@
+package OOPS;
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+
+class Dog extends Animal {
+
+   
+    void sound() {
+    	
+        System.out.println("Dog barks");
+    }
+}
+
+public class overriding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog d= new Dog();
+		d.sound();
+	}
+
+}

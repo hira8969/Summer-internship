dfc0a4a060b9c91f172f4088de0248261def6f48
hira8969 <hk2493200@gmail.com>
2026-05-27 22:15:25 +0530

    update
    update

--- test/src/arrays/twenty3.java
+++ test/src/arrays/twenty3.java
@@ -2,32 +2,40 @@ package arrays;
 
 public class twenty3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 int arr[] = {1, 2, 1, 3, 0, 4};
-		 secondSmall(arr);
-	        
-	}
-	public static void secondSmall(int arr[])
-	{
-		int smallest = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 1, 3, 0, 4};
+
+        secondSmall(arr);
+    }
+
+    public static void secondSmall(int arr[]) {
+
+    
+        if (arr.length < 2) {
+            System.out.println("Second smallest element not possible");
+            return;
+        }
+
+        int smallest = Integer.MAX_VALUE;
         int secondSmallest = Integer.MAX_VALUE;
 
         for (int num : arr) {
 
-            // Find smallest element
             if (num < smallest) {
                 secondSmallest = smallest;
                 smallest = num;
             }
 
-            // Find second smallest element
-            else if (num > smallest && num < secondSmallest) {
+            else if (num != smallest && num < secondSmallest) {
                 secondSmallest = num;
             }
         }
 
-        System.out.println("Second Smallest Element = " + secondSmallest);
-	}
-
+        if (secondSmallest == Integer.MAX_VALUE) {
+            System.out.println("No second smallest element");
+        } else {
+            System.out.println("Second Smallest Element = " + secondSmallest);
+        }
+    }
 }

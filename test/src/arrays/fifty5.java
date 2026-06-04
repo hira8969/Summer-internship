363d7cd8625bcf5b3043c5745fff8dd93d3f7e25
hira8969 <hk2493200@gmail.com>
2026-06-05 00:43:33 +0530

    update

--- test/src/arrays/fifty5.java
+++ test/src/arrays/fifty5.java
@@ -1,7 +1,7 @@
 package arrays;
 
 import java.util.Arrays;
-
+// 55. Write a Java program to sort array elements using the sort() method.   
 public class fifty5 {
 
     public static void main(String[] args) {

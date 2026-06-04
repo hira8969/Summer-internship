2bade2e96d6602ef48bd262130048727b39c3f85
hira8969 <hk2493200@gmail.com>
2026-06-05 00:47:23 +0530

    update

---
+++ test/src/arrays/fifty7.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class fifty7 {
+
+    public static void main(String[] args) {
+
+        char[] arr = {'R', 'A', 'C', 'D', 'B'};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Output: {");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("}");
+    }
+}

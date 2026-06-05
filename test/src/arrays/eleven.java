d57a5786efc84cc74f5aec51bb75e8c481e45048
hira8969 <hk2493200@gmail.com>
2026-06-05 11:35:35 +0530

    update

--- test/src/arrays/eleven.java
+++ test/src/arrays/eleven.java
@@ -20,7 +20,7 @@ public class eleven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,2,3,4,5,6};
+		int arr[]= {1,2,3,4,5,6,7,8,9,10};
 		System.out.print("Prime Numbers: ");
 
 		for (int num : arr) {

1bfdec708e7bf8a2d52cef59a6a06af0f89384bf
hira8969 <hk2493200@gmail.com>
2026-06-05 09:59:35 +0530

    update

--- test/src/arrays/fifty7.java
+++ test/src/arrays/fifty7.java
@@ -19,16 +19,12 @@ public class fifty7 {
             }
         }
 
-        System.out.print("Output: {");
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
 
-            if (i < arr.length - 1) {
-                System.out.print(", ");
-            }
+            
         }
 
-        System.out.println("}");
     }
 }

2b94cd35a128f82c6ac92ff0370c6381a24edf13
hira8969 <hk2493200@gmail.com>
2026-06-05 11:48:00 +0530

    code clean

--- test/src/arrays/four.java
+++ test/src/arrays/four.java
@@ -3,7 +3,7 @@ package arrays;
 public class four {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generat
 		int arr[]= {1,2,3,4,5,6};
 		int n= arr.length;
 		for(int i=0;i<n;i++)

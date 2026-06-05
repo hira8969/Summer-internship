59807cb7533ede4a98776b7133775a1ec75fc449
hira8969 <hk2493200@gmail.com>
2026-06-05 11:44:11 +0530

    clean code

--- test/src/arrays/five.java
+++ test/src/arrays/five.java
@@ -3,7 +3,7 @@ package arrays;
 public class five {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated 
+		
 		int arr[]= {1,2,3,4,5,6};
 		int n= arr.length;
 		for(int i=0;i<n;i++)

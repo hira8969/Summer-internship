229887a34a52e04fcecceda4e9d84da3670dbb0a
hira8969 <hk2493200@gmail.com>
2026-06-05 11:40:54 +0530

    update

--- test/src/arrays/first.java
+++ test/src/arrays/first.java
@@ -3,7 +3,7 @@ package arrays;
 public class first {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		String []arr= {"Hira","Lal","Kumar","Raja","Arjun","Prashant"};
 		int n= arr.length ;
 		for(int i= n-1; i>0 ;i--)

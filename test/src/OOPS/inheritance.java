63140db9f27994cfc77f2a3b7c67dc8eff190f8d
Hira lal Kumar <hk2493200@gmail.com>
2026-06-07 22:14:03 +0530

    Update inheritance.java

--- test/src/OOPS/inheritance.java
+++ test/src/OOPS/inheritance.java
@@ -14,7 +14,7 @@ class Car extends Vehicle {
 public class inheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated 
+		// TODO Auto-gener
 		Car c= new Car();
 		c.start();  // call parent call method 
 		c.drive();  // call child class method 

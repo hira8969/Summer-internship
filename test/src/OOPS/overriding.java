a60b95d868484ad3e405b724c0bbe8b041e4c1e8
Hira lal Kumar <hk2493200@gmail.com>
2026-06-07 22:09:39 +0530

    Update overriding.java

--- test/src/OOPS/overriding.java
+++ test/src/OOPS/overriding.java
@@ -19,7 +19,7 @@ class Dog extends Animal {
 public class overriding {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method 
+		// TODO Auto-generated 
 		Dog d= new Dog();
 		d.sound();
 	}

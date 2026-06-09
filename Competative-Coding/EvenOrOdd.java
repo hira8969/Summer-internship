7ce22ad04d3c734adac0f75f91cb826ba781bcde
hira8969 <hk2493200@gmail.com>
2026-06-09 10:51:37 +0530

    Compatative-coding

---
+++ Competative-Coding/EvenOrOdd.java
@@ -0,0 +1,11 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 7; // You can change this number to test with different values
+
+        if ((number & 1) == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}

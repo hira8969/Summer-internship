078e3ae668676276ed50119361cebad5b6e50e65
hira8969 <hk2493200@gmail.com>
2026-06-10 09:15:32 +0530

    solve leetcode problem

---
+++ Competative-Coding/Arrays/NextPermutation.java
@@ -0,0 +1,69 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NextPermutation {
+
+    public static void nextPermutation(int[] nums) {
+
+        int n = nums.length;
+
+        // Step 1: Find the pivot
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: Find the next greater element and swap
+        if (i >= 0) {
+            int j = n - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Original Array: " + Arrays.toString(nums));
+
+        nextPermutation(nums);
+
+        System.out.println("Next Permutation: " + Arrays.toString(nums));
+
+        sc.close();
+    }
+}

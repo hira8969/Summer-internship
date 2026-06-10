91032b482ae554b790647f73dd4b2c3ba6492d2a
hira8969 <hk2493200@gmail.com>
2026-06-10 09:21:14 +0530

    update

---
+++ Competative-Coding/Arrays/FirstAndLast.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class FirstAndLast {
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private static int findFirst(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private static int findLast(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int[] result = searchRange(nums, target);
+
+        System.out.println("Output: [" + result[0] + ", " + result[1] + "]");
+
+        sc.close();
+    }
+}

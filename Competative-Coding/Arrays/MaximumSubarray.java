2b3b8fd60d2dce8db30c5eb2ca8c08d0865feeb2
hira8969 <hk2493200@gmail.com>
2026-06-12 10:45:18 +0530

    solve

---
+++ Competative-Coding/Arrays/MaximumSubarray.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.*;
+
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int ans = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum = " + ans);
+    }
+}

f6730adb90dc1561f5a4f201bffd230bf16fcac9
hira8969 <hk2493200@gmail.com>
2026-06-15 09:55:58 +0530

    MejorityElement

---
+++ Competative-Coding/Arrays/MejorityElement.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class MejorityElement {
+
+    public static int majorityElement(int[] nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+
+        int result = majorityElement(nums);
+
+        System.out.println(result);
+    }
+}

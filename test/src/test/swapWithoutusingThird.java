845c8b366058e9b10207a3bb519ed44516d9695e
Hira lal Kumar <hk2493200@gmail.com>
2026-06-17 20:30:42 +0530

    Update swapWithoutusingThird.java

--- test/src/test/swapWithoutusingThird.java
+++ test/src/test/swapWithoutusingThird.java
@@ -3,7 +3,7 @@ package test;
 public class swapWithoutusingThird {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+		// TODO Auto-generated method st
 		int a = 10;
         int b = 20;
 

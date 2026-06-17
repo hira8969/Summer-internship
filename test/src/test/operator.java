ac8a80a3430f1b23698ca150ea43e1a8b4288dac
Hira lal Kumar <hk2493200@gmail.com>
2026-06-17 20:23:15 +0530

    Update operator.java

--- test/src/test/operator.java
+++ test/src/test/operator.java
@@ -3,7 +3,7 @@ package test;
 public class operator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated me
+		// TODO Auto-generated m
 		int a=10;
 		int b=20;
 		System.out.println("Arithmetic Operator");

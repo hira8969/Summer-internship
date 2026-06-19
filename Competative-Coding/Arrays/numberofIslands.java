586b4f64f84d52e2ea1dca3a037c9f560b86c9c5
hira8969 <hk2493200@gmail.com>
2026-06-19 11:32:21 +0530

    solved

---
+++ Competative-Coding/Arrays/numberofIslands.java
@@ -0,0 +1,72 @@
+package Arrays;
+
+
+    
+import java.util.*;
+
+public class numberofIslands {
+
+    public static int numIslands(char[][] grid) {
+
+        int count = 0;
+
+        int rows = grid.length;
+
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == '1') {
+
+                    count++;
+
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void dfs(char[][] grid, int row, int col) {
+
+        int rows = grid.length;
+
+        int cols = grid[0].length;
+
+        if (row < 0 || col < 0 ||
+            row >= rows || col >= cols ||
+            grid[row][col] == '0') {
+
+            return;
+        }
+
+        grid[row][col] = '0';
+
+        dfs(grid, row + 1, col);
+
+        dfs(grid, row - 1, col);
+
+        dfs(grid, row, col + 1);
+
+        dfs(grid, row, col - 1);
+    }
+
+    public static void main(String[] args) {
+
+        char[][] grid = {
+
+            {'1','1','0','0','0'},
+
+            {'1','1','0','0','0'},
+
+            {'0','0','1','0','0'},
+
+            {'0','0','0','1','1'}
+        };
+
+        System.out.println(numIslands(grid));
+    }
+}

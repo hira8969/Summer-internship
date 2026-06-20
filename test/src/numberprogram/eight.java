d76492c01f2e6d574c48d47ec11a8cba98488170
Hira lal Kumar <hk2493200@gmail.com>
2026-06-20 12:56:53 +0530

    Update eight.java

--- test/src/numberprogram/eight.java
+++ test/src/numberprogram/eight.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class eight {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method
 		Scanner sc= new Scanner(System.in);
 		System.out.print("Enter a nuumber: ");
 		int num= sc.nextInt();

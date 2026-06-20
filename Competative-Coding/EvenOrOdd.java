363829fd6b16e05697887dd89b185146f205a9d0
Hira lal Kumar <hk2493200@gmail.com>
2026-06-20 13:03:02 +0530

    Update EvenOrOdd.java

--- Competative-Coding/EvenOrOdd.java
+++ Competative-Coding/EvenOrOdd.java
@@ -1,6 +1,6 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
-        int number = 7; // You can change this number to test with different 
+        int number = 7; // You can change this number to test with  
         if ((number & 1) == 0) {
             System.out.println(number + " is even.");
         } else {

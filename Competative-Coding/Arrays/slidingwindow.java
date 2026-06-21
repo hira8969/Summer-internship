e857de652c429e41e97694040d18b5bea337461f
hira8969 <hk2493200@gmail.com>
2026-06-21 20:38:04 +0530

    update

---
+++ Competative-Coding/Arrays/slidingwindow.java
@@ -0,0 +1,68 @@
+package Arrays;
+
+public class slidingwindow {
+    
+import java.util.*;
+
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+
+        int[] result = new int[n - k + 1];
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove indices outside current window
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+
+            // Remove smaller elements
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            // Add current index
+            deque.offerLast(i);
+
+            // Store maximum when window size becomes k
+            if (i >= k - 1) {
+                result[index++] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // Input array elements
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Input k
+        int k = sc.nextInt();
+
+        int[] ans = maxSlidingWindow(nums, k);
+
+        // Print output
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}

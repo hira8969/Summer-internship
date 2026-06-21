ad492d1f42556a5b01419e542890042080f54e25
hira8969 <hk2493200@gmail.com>
2026-06-21 21:04:23 +0530

    ad

---
+++ Competative-Coding/Arrays/SearchIn2dmatrix.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+
+    
+public class SearchIn2dmatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+
+            if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+
+        int target = 5;
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+}

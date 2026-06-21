6d0982bc51225256aa3a77667bce5e4970a92034
hira8969 <hk2493200@gmail.com>
2026-06-21 21:10:44 +0530

    Create Singlenumber2.java

---
+++ Competative-Coding/Arrays/Singlenumber2.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+
+    
+import java.util.Arrays;
+
+public class Singlenumber2 {
+    public static int[] singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int rightMostBit = xor & (-xor);
+
+        int first = 0;
+        int second = 0;
+
+        for (int num : nums) {
+
+            if ((num & rightMostBit) == 0) {
+                first ^= num;
+            } else {
+                second ^= num;
+            }
+        }
+
+        return new int[]{first, second};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 1, 3, 2, 5};
+
+        int[] ans = singleNumber(nums);
+
+        System.out.println(Arrays.toString(ans));
+    }
+}

e8ec837c3787cae1e1fc7782d8014777e84b3dc4
hira8969 <hk2493200@gmail.com>
2026-06-22 19:20:24 +0530

    Update InsertionofTwoarr.java

--- Competative-Coding/Arrays/InsertionofTwoarr.java
+++ Competative-Coding/Arrays/InsertionofTwoarr.java
@@ -2,4 +2,46 @@ package Arrays;
 
 public class InsertionofTwoarr {
     
+import java.util.*;
+
+
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums2) {
+
+            if (map.containsKey(num) && map.get(num) > 0) {
+
+                list.add(num);
+
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+
+        int[] result = intersect(nums1, nums2);
+
+        System.out.println(Arrays.toString(result));
+    }
 }

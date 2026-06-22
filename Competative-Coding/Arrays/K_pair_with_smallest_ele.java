cea20b44aac47c5dbcdab2e72c296d33723d783f
hira8969 <hk2493200@gmail.com>
2026-06-22 19:40:06 +0530

    Update K_pair_with_smallest_ele.java

--- Competative-Coding/Arrays/K_pair_with_smallest_ele.java
+++ Competative-Coding/Arrays/K_pair_with_smallest_ele.java
@@ -2,3 +2,61 @@ package Arrays;
 
 public class K_pair_with_smallest_ele {
     
+import java.util.*;
+
+public class Main {
+
+    public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
+            return ans;
+        }
+
+        PriorityQueue<int[]> pq =
+                new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (int i = 0; i < Math.min(nums1.length, k); i++) {
+
+            pq.offer(new int[]{
+                    nums1[i] + nums2[0],
+                    i,
+                    0
+            });
+        }
+
+        while (k > 0 && !pq.isEmpty()) {
+
+            int[] curr = pq.poll();
+
+            int i = curr[1];
+            int j = curr[2];
+
+            ans.add(Arrays.asList(nums1[i], nums2[j]));
+
+            k--;
+
+            if (j + 1 < nums2.length) {
+
+                pq.offer(new int[]{
+                        nums1[i] + nums2[j + 1],
+                        i,
+                        j + 1
+                });
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 7, 11};
+        int[] nums2 = {2, 4, 6};
+
+        int k = 3;
+
+        System.out.println(kSmallestPairs(nums1, nums2, k));
+    }
+}

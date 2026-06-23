e877e47a1323fad881537d2130e1baeed5117838
hira8969 <hk2493200@gmail.com>
2026-06-23 22:15:35 +0530

    update

--- Competative-Coding/Arrays/Non.java
+++ Competative-Coding/Arrays/Non.java
@@ -3,3 +3,46 @@ package Arrays;
 public class Non-overlappingIntervals {
     
 }
+import java.util.Arrays;
+
+public class Main {
+
+    public static int eraseOverlapIntervals(int[][] intervals) {
+
+        // Sort by ending time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int remove = 0;
+
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            // Overlap
+            if (intervals[i][0] < prevEnd) {
+
+                remove++;
+
+            } else {
+
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return remove;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {
+                {1, 2},
+                {2, 3},
+                {3, 4},
+                {1, 3}
+        };
+
+        int ans = eraseOverlapIntervals(intervals);
+
+        System.out.println("Minimum intervals to remove = " + ans);
+    }
+}

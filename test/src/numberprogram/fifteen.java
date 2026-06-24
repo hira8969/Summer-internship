a6e1ab79540af9deeea89906d9ddc171ccb77756
Hira lal Kumar <hk2493200@gmail.com>
2026-06-24 23:38:12 +0530

    Update fifteen.java

--- test/src/numberprogram/fifteen.java
+++ test/src/numberprogram/fifteen.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class fifteen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method 
 		Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");

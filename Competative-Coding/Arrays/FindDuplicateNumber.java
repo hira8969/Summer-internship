c2adb8700ee31fb66484f109ee9d4a523ea36cd6
hira8969 <hk2493200@gmail.com>
2026-06-25 16:26:55 +0530

    update

--- Competative-Coding/Arrays/FindDuplicateNumber.java
+++ Competative-Coding/Arrays/FindDuplicateNumber.java
@@ -3,4 +3,35 @@ package Arrays;
 public class FindDuplicateNumber {
 
     
+public class FindDuplicateNumber {
+
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Detect cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find duplicate
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 4, 2, 2};
+        int[] nums2 = {3, 1, 3, 4, 2};
+        int[] nums3 = {3, 3, 3, 3, 3};
+
+        System.out.println(findDuplicate(nums1)); // 2
+        System.out.println(findDuplicate(nums2)); // 3
+        System.out.println(findDuplicate(nums3)); // 3
+    }
 }

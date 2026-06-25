9e34061c80a096279b93b3d5bcd77c2d6e154ec3
hira8969 <hk2493200@gmail.com>
2026-06-25 19:03:17 +0530

    up

--- Competative-Coding/Arrays/Pattern132.java
+++ Competative-Coding/Arrays/Pattern132.java
@@ -2,4 +2,40 @@ package Arrays;
 
 public class Pattern132 {
     
+import java.util.Stack;
+
+public class Main {
+
+    public static boolean find132pattern(int[] nums) {
+
+        Stack<Integer> stack = new Stack<>();
+        int third = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            if (nums[i] < third) {
+                return true;
+            }
+
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                third = stack.pop();
+            }
+
+            stack.push(nums[i]);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3, 4};
+        System.out.println(find132pattern(nums1)); // false
+
+        int[] nums2 = {3, 1, 4, 2};
+        System.out.println(find132pattern(nums2)); // true
+
+        int[] nums3 = {-1, 3, 2, 0};
+        System.out.println(find132pattern(nums3)); // true
+    }
 }

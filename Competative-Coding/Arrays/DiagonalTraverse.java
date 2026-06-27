2cc1b250c62f84d618fb5d2c8b269cb865313860
hira8969 <hk2493200@gmail.com>
2026-06-27 22:44:08 +0530

    update

--- Competative-Coding/Arrays/DiagonalTraverse.java
+++ Competative-Coding/Arrays/DiagonalTraverse.java
@@ -2,4 +2,54 @@ package Arrays;
 
 public class DiagonalTraverse {
     
+import java.util.*;
+
+public class Main {
+
+    public static int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {
+            result[i] = mat[row][col];
+
+            if ((row + col) % 2 == 0) { // Moving Up
+                if (col == n - 1) {
+                    row++;
+                } else if (row == 0) {
+                    col++;
+                } else {
+                    row--;
+                    col++;
+                }
+            } else { // Moving Down
+                if (row == m - 1) {
+                    col++;
+                } else if (col == 0) {
+                    row++;
+                } else {
+                    row++;
+                    col--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[] ans = findDiagonalOrder(mat);
+
+        System.out.println(Arrays.toString(ans));
+    }
 }

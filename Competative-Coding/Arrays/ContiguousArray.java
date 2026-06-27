799142d325a6131379515861a5e9a9b16e86e4dd
hira8969 <hk2493200@gmail.com>
2026-06-27 23:15:32 +0530

    up

--- Competative-Coding/Arrays/ContiguousArray.java
+++ Competative-Coding/Arrays/ContiguousArray.java
@@ -2,4 +2,39 @@ package Arrays;
 
 public class ContiguousArray {
     
+import java.util.*;
+
+public class Main {
+
+    public static int findMaxLength(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0) {
+                sum--;
+            } else {
+                sum++;
+            }
+
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 1, 1, 0};
+
+        System.out.println(findMaxLength(nums));
+    }
 }

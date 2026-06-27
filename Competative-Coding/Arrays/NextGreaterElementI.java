0338a09e90e41d70a2d4d469330f193dfe69d87b
hira8969 <hk2493200@gmail.com>
2026-06-27 22:38:44 +0530

    update

--- Competative-Coding/Arrays/NextGreaterElementI.java
+++ Competative-Coding/Arrays/NextGreaterElementI.java
@@ -1,5 +1,41 @@
 package Arrays;
-public class NextGreaterElementI {
 
-    
+
+import java.util.*;
+
+public class NextGreaterElementI {
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Find next greater element for nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Remaining elements have no greater element
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        int[] result = nextGreaterElement(nums1, nums2);
+
+        System.out.println("Output: " + Arrays.toString(result));
+    }
 }

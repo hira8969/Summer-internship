cc0e10d457c2c426d9ebf7d405bb7af51120ba73
hira8969 <hk2493200@gmail.com>
2026-06-28 23:09:06 +0530

    maximumElement

---
+++ Competative-Coding/Arrays/maximumElement.java
@@ -0,0 +1,28 @@
+
+
+import java.util.*;
+
+public class  {
+
+    public static int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
+        Arrays.sort(arr);
+
+        arr[0] = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+        }
+
+        return arr[arr.length - 1];
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {100, 1, 1000};
+
+        System.out.println(
+            maximumElementAfterDecrementingAndRearranging(arr)
+        );
+    }
+}

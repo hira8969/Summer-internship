1a917e1aaf010257253990eba0022ae7c8ed880c
hira8969 <hk2493200@gmail.com>
2026-06-30 12:05:04 +0530

    update all

--- Competative-Coding/Arrays/CountofSmallerNumbersAfterSelf.java
+++ Competative-Coding/Arrays/CountofSmallerNumbersAfterSelf.java
@@ -2,4 +2,79 @@ package Arrays;
 
 public class CountofSmallerNumbersAfterSelf {
     
+import java.util.*;
+
+public class Main {
+
+    private static int[] count;
+    private static int[] indexes;
+
+    public static List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+
+        count = new int[n];
+        indexes = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            indexes[i] = i;
+        }
+
+        mergeSort(nums, 0, n - 1);
+
+        List<Integer> ans = new ArrayList<>();
+        for (int x : count) {
+            ans.add(x);
+        }
+
+        return ans;
+    }
+
+    private static void mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        merge(nums, left, mid, right);
+    }
+
+    private static void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        int rightCount = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[indexes[j]] < nums[indexes[i]]) {
+                temp[k++] = indexes[j++];
+                rightCount++;
+            } else {
+                count[indexes[i]] += rightCount;
+                temp[k++] = indexes[i++];
+            }
+        }
+
+        while (i <= mid) {
+            count[indexes[i]] += rightCount;
+            temp[k++] = indexes[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = indexes[j++];
+        }
+
+        for (int p = 0; p < temp.length; p++) {
+            indexes[left + p] = temp[p];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 6, 1};
+
+        System.out.println(countSmaller(nums));
+    }
 }

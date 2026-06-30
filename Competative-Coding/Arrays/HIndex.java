52bce2ad399d05ce236a827cc34202470fac4fd3
hira8969 <hk2493200@gmail.com>
2026-06-30 11:59:01 +0530

    update

--- Competative-Coding/Arrays/HIndex.java
+++ Competative-Coding/Arrays/HIndex.java
@@ -2,4 +2,29 @@ package Arrays;
 
 public class HIndex {
     
+import java.util.Arrays;
+
+public class Main {
+
+    public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+
+        System.out.println("H-Index = " + hIndex(citations));
+    }
 }

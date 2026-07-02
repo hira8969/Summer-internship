415c529508c87451214f1e1aab9590b9eb03243f
hira8969 <hk2493200@gmail.com>
2026-07-02 10:35:24 +0530

    630. Course Schedule III

--- Competative-Coding/Arrays/CourseScheduleIII.java
+++ Competative-Coding/Arrays/CourseScheduleIII.java
@@ -2,4 +2,44 @@ package Arrays;
 
 public class CourseScheduleIII {
     
+import java.util.*;
+
+public class Main {
+
+    public static int scheduleCourse(int[][] courses) {
+
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        int totalTime = 0;
+
+        for (int[] course : courses) {
+
+            int duration = course[0];
+            int lastDay = course[1];
+
+            totalTime += duration;
+            maxHeap.offer(duration);
+
+            if (totalTime > lastDay) {
+                totalTime -= maxHeap.poll();
+            }
+        }
+
+        return maxHeap.size();
+    }
+
+    public static void main(String[] args) {
+
+        int[][] courses = {
+                {100, 200},
+                {200, 1300},
+                {1000, 1250},
+                {2000, 3200}
+        };
+
+        System.out.println(scheduleCourse(courses));
+    }
 }

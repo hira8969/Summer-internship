bd0c96b2721367654d49770a2773118f41fd981c
hira8969 <hk2493200@gmail.com>
2026-07-02 19:39:03 +0530

    658. Find K Closest Elements

--- Competative-Coding/Arrays/FindKClosestElements.java
+++ Competative-Coding/Arrays/FindKClosestElements.java
@@ -2,4 +2,41 @@ package Arrays;
 
 public class FindKClosestElements {
     
+import java.util.*;
+
+public class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - k;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        int x = 3;
+
+        List<Integer> ans = sol.findClosestElements(arr, k, x);
+
+        System.out.println(ans);
+    }
 }

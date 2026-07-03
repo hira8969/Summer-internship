6b8976f15d17adf67fbcf3c0f7eff8144646dd12
hira8969 <hk2493200@gmail.com>
2026-07-03 14:12:58 +0530

    6. Zigzag Conversion

--- Competative-Coding/String/ZigzagConversion.java
+++ Competative-Coding/String/ZigzagConversion.java
@@ -2,4 +2,53 @@ package String;
 
 public class ZigzagConversion {
     
+import java.util.*;
+
+public class Solution {
+
+    public static String convert(String s, int numRows) {
+
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currRow = 0;
+        boolean goingDown = false;
+
+        for (char ch : s.toCharArray()) {
+
+            rows[currRow].append(ch);
+
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+
+        System.out.println(convert(s, numRows)); // PAHNAPLSIIGYIR
+
+        System.out.println(convert("PAYPALISHIRING", 4)); // PINALSIGYAHRPI
+        System.out.println(convert("A", 1)); // A
+    }
 }

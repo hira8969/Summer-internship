f087de5a37a4d4f1d9ecedda4b31a69b208ba929
hira8969 <hk2493200@gmail.com>
2026-07-03 17:51:58 +0530

    Integer to Roman

--- Competative-Coding/String/IntegertoRoman.java
+++ Competative-Coding/String/IntegertoRoman.java
@@ -2,4 +2,37 @@ package String;
 
 public class IntegertoRoman {
     
+public class Main {
+
+    public static String intToRoman(int num) {
+
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                ans.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(3749)); // MMMDCCXLIX
+        System.out.println(intToRoman(58));   // LVIII
+        System.out.println(intToRoman(1994)); // MCMXCIV
+    }
 }

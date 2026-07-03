606d7e43c22015646bb6cf8b71ba4ba51fae462f
hira8969 <hk2493200@gmail.com>
2026-07-03 14:16:50 +0530

    8. String to Integer (atoi)

--- Competative-Coding/String/StringtoInteger.java
+++ Competative-Coding/String/StringtoInteger.java
@@ -2,4 +2,53 @@ package String;
 
 public class StringtoInteger {
     
+import java.util.*;
+
+public class Main {
+
+    public static int myAtoi(String s) {
+
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        int sign = 1;
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        long num = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+
+            num = num * 10 + (s.charAt(i) - '0');
+
+            if (sign * num > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (sign * num < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int) (sign * num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi("42"));          // 42
+        System.out.println(myAtoi("   -042"));    // -42
+        System.out.println(myAtoi("1337c0d3"));   // 1337
+        System.out.println(myAtoi("0-1"));        // 0
+        System.out.println(myAtoi("words 987"));  // 0
+    }
 }

9248cec3e3bb7662dd0759328d9ffc25f34153c5
hira8969 <hk2493200@gmail.com>
2026-07-03 18:04:54 +0530

    Valid Parentheses

--- Competative-Coding/String/ValidParentheses.java
+++ Competative-Coding/String/ValidParentheses.java
@@ -2,4 +2,42 @@ package String;
 
 public class ValidParentheses {
     
+import java.util.*;
+
+public class Main {
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));      // true
+        System.out.println(isValid("()[]{}"));  // true
+        System.out.println(isValid("(]"));      // false
+        System.out.println(isValid("([])"));    // true
+        System.out.println(isValid("([)]"));    // false
+    }
 }

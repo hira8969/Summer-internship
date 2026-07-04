9a6828db0d1ed86a326adcbe7c4c2b792cd57c6f
hira8969 <hk2493200@gmail.com>
2026-07-04 20:18:31 +0530

    Longest Valid Parentheses

--- Competative-Coding/String/LongestValidParentheses.java
+++ Competative-Coding/String/LongestValidParentheses.java
@@ -2,4 +2,45 @@ package String;
 
 public class LongestValidParentheses {
     
+import java.util.*;
+
+public class Main {
+
+    public static int longestValidParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter parentheses string: ");
+        String s = sc.nextLine();
+
+        System.out.println("Longest Valid Length = "
+                + longestValidParentheses(s));
+
+        sc.close();
+    }
 }

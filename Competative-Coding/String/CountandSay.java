e7526e35a5c36f22c513f8ddc39a576c5cb41d61
hira8969 <hk2493200@gmail.com>
2026-07-04 20:24:12 +0530

    Count and Say

--- Competative-Coding/String/CountandSay.java
+++ Competative-Coding/String/CountandSay.java
@@ -2,4 +2,48 @@ package String;
 
 public class CountandSay {
     
+import java.util.*;
+
+public class Main {
+
+    public static String countAndSay(int n) {
+
+        String result = "1";
+
+        for (int i = 2; i <= n; i++) {
+
+            StringBuilder temp = new StringBuilder();
+            int count = 1;
+
+            for (int j = 1; j < result.length(); j++) {
+
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+                    temp.append(count);
+                    temp.append(result.charAt(j - 1));
+                    count = 1;
+                }
+            }
+
+            temp.append(count);
+            temp.append(result.charAt(result.length() - 1));
+
+            result = temp.toString();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.println(countAndSay(n));
+
+        sc.close();
+    }
 }

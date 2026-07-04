e02f2154604858bd73ff769aee65f914fd32263e
hira8969 <hk2493200@gmail.com>
2026-07-04 20:01:36 +0530

    Generate Parentheses

--- Competative-Coding/String/GenerateParentheses.java
+++ Competative-Coding/String/GenerateParentheses.java
@@ -2,4 +2,46 @@ package String;
 
 public class GenerateParentheses {
     
+import java.util.*;
+
+public class Main {
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private static void backtrack(List<String> result, String current,
+                                  int open, int close, int n) {
+
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        List<String> ans = generateParenthesis(n);
+
+        System.out.println("Valid Parentheses Combinations:");
+        for (String s : ans) {
+            System.out.println(s);
+        }
+
+        sc.close();
+    }
 }

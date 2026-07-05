be7d7cb867fe993cd3810d97de8fca67e2bf3bbb
hira8969 <hk2493200@gmail.com>
2026-07-05 13:58:05 +0530

    Valid Number

--- Competative-Coding/String/ValidNumber.java
+++ Competative-Coding/String/ValidNumber.java
@@ -2,4 +2,19 @@ package String;
 
 public class ValidNumber {
     
+public class Main {
+
+    public static boolean isNumber(String s) {
+        return s.matches("[+-]?((\\d+\\.?\\d*)|(\\.\\d+))([eE][+-]?\\d+)?");
+    }
+
+    public static void main(String[] args) {
+        String s = "2e10";
+
+        if (isNumber(s)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
 }

32337adb75342691e7b890e68c708256c5f59031
hira8969 <hk2493200@gmail.com>
2026-07-05 22:10:19 +0530

    word break

--- Competative-Coding/String/WordBreak.java
+++ Competative-Coding/String/WordBreak.java
@@ -4,4 +4,33 @@ package String;
 
 public class WordBreak {
     
+import java.util.*;
+
+public class Main {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        List<String> wordDict = Arrays.asList("leet", "code");
+
+        System.out.println(wordBreak(s, wordDict));
+    }
 }

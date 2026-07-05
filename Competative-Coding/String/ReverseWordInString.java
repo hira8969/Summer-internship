9402ea32ddee0815e7cb651f3b0bdc83f1e5f781
hira8969 <hk2493200@gmail.com>
2026-07-05 22:14:35 +0530

    updagte

--- Competative-Coding/String/ReverseWordInString.java
+++ Competative-Coding/String/ReverseWordInString.java
@@ -2,4 +2,26 @@ package String;
 
 public class ReverseWordInString {
     
+public class Main {
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            ans.append(words[i]);
+
+            if (i != 0) {
+                ans.append(" ");
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("the sky is blue"));
+        System.out.println(reverseWords("  hello world  "));
+        System.out.println(reverseWords("a good   example"));
+    }
 }

1879cc913e1dcd0b1385b56946b9cbc8ab08a3f5
hira8969 <hk2493200@gmail.com>
2026-07-05 14:04:57 +0530

    125. Valid Palindrome

--- Competative-Coding/String/ValidPalindrome.java
+++ Competative-Coding/String/ValidPalindrome.java
@@ -1,7 +1,6 @@
 package String;
 //125. Valid Palindrome
-public class ValidPalindrome {
-    
+public class ValidPalindrome {   
 
     public static boolean isPalindrome(String s) {
         int left = 0;

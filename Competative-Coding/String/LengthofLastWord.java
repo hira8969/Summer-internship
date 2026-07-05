064a99c00cc9b206f4ea5bd1b6f09fecde5ba951
hira8969 <hk2493200@gmail.com>
2026-07-05 13:54:50 +0530

    Length of Last Word

--- Competative-Coding/String/LengthofLastWord.java
+++ Competative-Coding/String/LengthofLastWord.java
@@ -2,13 +2,25 @@
 package String;
 
 public class LengthofLastWord {
+    public class Main {
     public static int lengthOfLastWord(String s) {
-        String[] words = s.trim().split(" ");
-        return words[words.length - 1].length();
+        int len = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
     }
 
     public static void main(String[] args) {
-        String s = "Hello World";
-        System.out.println("Length of last word: " + lengthOfLastWord(s));
+        String s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
     }
 }

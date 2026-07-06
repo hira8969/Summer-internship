97bd74167334cb9d61ab650c95f3ed96b94e6aa7
hira8969 <hk2493200@gmail.com>
2026-07-06 11:21:29 +0530

    Shortest Palindrome

--- Competative-Coding/String/ShortestPalindrome.java
+++ Competative-Coding/String/ShortestPalindrome.java
@@ -2,4 +2,34 @@ package String;
 
 public class ShortestPalindrome {
     
+class Solution {
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+
+        // Find the longest palindromic prefix
+        int end = n;
+        while (end > 0) {
+            if (isPalindrome(s, 0, end - 1)) {
+                break;
+            }
+            end--;
+        }
+
+        String suffix = s.substring(end);
+        StringBuilder result = new StringBuilder(suffix).reverse();
+        result.append(s);
+
+        return result.toString();
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }

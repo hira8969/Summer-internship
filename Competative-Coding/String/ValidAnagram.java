40313c06fc76ca74b16a062e9c0f34b3c3df9eea
hira8969 <hk2493200@gmail.com>
2026-07-06 11:43:12 +0530

    Valid Anagram

--- Competative-Coding/String/ValidAnagram.java
+++ Competative-Coding/String/ValidAnagram.java
@@ -2,4 +2,39 @@ package String;
 
 public class ValidAnagram {
     
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int x : freq) {
+            if (x != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        System.out.println(isAnagram(s, t));
+
+        sc.close();
+    }
 }

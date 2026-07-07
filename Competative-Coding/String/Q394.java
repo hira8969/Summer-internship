93fbfe89b5ada8c9244e13d6f763af37ea843ad8
hira8969 <hk2493200@gmail.com>
2026-07-07 21:46:36 +0530

    394. Decode String

--- Competative-Coding/String/Q394.java
+++ Competative-Coding/String/Q394.java
@@ -2,4 +2,48 @@ package String;
 //394. Decode String
 public class Q394 {
     
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "3[a2[c]]";
+
+        System.out.println(decodeString(s));
+    }
+
+    public static String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+
+        StringBuilder current = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(current);
+
+                k = 0;
+                current = new StringBuilder();
+            }
+            else if (ch == ']') {
+                int repeat = countStack.pop();
+                StringBuilder prev = stringStack.pop();
+
+                for (int i = 0; i < repeat; i++) {
+                    prev.append(current);
+                }
+
+                current = prev;
+            }
+            else {
+                current.append(ch);
+            }
+        }
+
+        return current.toString();
+    }
 }

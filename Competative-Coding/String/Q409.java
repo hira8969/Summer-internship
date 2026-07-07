716d59e5b13d80be73b1c97d7556f91c5dded554
hira8969 <hk2493200@gmail.com>
2026-07-07 22:02:27 +0530

    409. Longest Palindrome

--- Competative-Coding/String/Q409.java
+++ Competative-Coding/String/Q409.java
@@ -2,4 +2,30 @@ package String;
 //409. Longest Palindrome
 public class Q409 {
     
+public class Main {
+    public static int longestPalindrome(String s) {
+        int[] freq = new int[128];
+
+        for (char c : s.toCharArray()) {
+            freq[c]++;
+        }
+
+        int length = 0;
+        boolean hasOdd = false;
+
+        for (int count : freq) {
+            length += (count / 2) * 2;
+
+            if (count % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+
+        return hasOdd ? length + 1 : length;
+    }
+
+    public static void main(String[] args) {
+        String s = "abccccdd";
+        System.out.println(longestPalindrome(s));
+    }
 }

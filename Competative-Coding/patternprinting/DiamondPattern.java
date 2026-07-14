ce2c27025ccc5f1baf8aedbddb70479ab45ce8e0
hira8969 <hk2493200@gmail.com>
2026-07-15 01:22:56 +0530

    add lower half

--- Competative-Coding/patternprinting/DiamondPattern.java
+++ Competative-Coding/patternprinting/DiamondPattern.java
@@ -4,7 +4,7 @@ public class DiamondPattern {
     public static void main(String[] args) {
         int n = 5;
 
-        // Upper Half
+        // Upper Half pyramid pattern
         for (int i = 1; i <= n; i++) {
             for (int j = i; j < n; j++) {
                 System.out.print(" ");
@@ -16,7 +16,18 @@ public class DiamondPattern {
 
             System.out.println();
         }
+        // Lower Half inverted pattern
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
 
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
         
     }
 }

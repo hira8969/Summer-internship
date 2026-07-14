054a6842c41d9cdb88281f1a25d7ef5a70a4c234
hira8969 <hk2493200@gmail.com>
2026-07-15 01:16:05 +0530

    print pyramid pattern

---
+++ Competative-Coding/patternprinting/DiamondPattern.java
@@ -0,0 +1,22 @@
+package patternprinting;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        
+    }
+}

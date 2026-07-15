e414add0962eedd42007977fec3c00ce25e7aea9
hira8969 <hk2493200@gmail.com>
2026-07-15 20:07:52 +0530

    solve sum of digit problem

--- Competative-Coding/CommonProblem/sumofdigit.java
+++ Competative-Coding/CommonProblem/sumofdigit.java
@@ -1,5 +1,23 @@
 package CommonProblem;
 
+import java.util.Scanner;
+
 public class sumofdigit {
-    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number here: ");
+        int num= sc.nextInt();
+        sumofdigit1(num);
+    }
+    public static void sumofdigit1(int n)
+    {
+        int sum=0;
+        while(n>0)
+        {
+            int last= n%10;
+            sum = (sum+ last) ;
+            n=n/10;
+        }
+        System.out.println("sum of digit: "+ sum);
+    }
 }

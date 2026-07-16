216767e6bdf5df1a88cb3a85caea9d9549109910
hira8969 <hk2493200@gmail.com>
2026-07-16 19:18:10 +0530

    solve but small bug

--- Competative-Coding/CommonProblem/Fibonacci.java
+++ Competative-Coding/CommonProblem/Fibonacci.java
@@ -6,19 +6,23 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n= sc.nextInt();
-        fibo(n);
+        int x= fibo(n);
+        System.out.println(x);
     }
-    public static void fibo(int n)
+    public static int fibo(int n)
     {
-        int first=0;
-        int second=1;
-        int next;
-        for(int i=1;i<=n;i++)
-        {
-            next= first + second;
-            first= second;
-            second= next;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int next = first + second;
+            first = second;
+            second = next;
         }
-        System.out.println("fibonacci number is : "+ next);
+        return second;
+
     }
 }

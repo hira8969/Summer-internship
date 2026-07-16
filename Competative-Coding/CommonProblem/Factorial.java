45cd101632f68de946bd564d14c9e2b751c50045
hira8969 <hk2493200@gmail.com>
2026-07-16 19:04:44 +0530

    Factorial

--- Competative-Coding/CommonProblem/Factorial.java
+++ Competative-Coding/CommonProblem/Factorial.java
@@ -1,3 +1,23 @@
+package CommonProblem;
+
+import java.util.Scanner;
+
 public class Factorial {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number Here: ");
+        int n=sc.nextInt();
+        System.out.println("Factorial of a number is: "+ fact(n));
+    }
+    public static long fact(int n)
+    {
+        int fac=0;
+        while(n>0)
+        {
+            int last = n%10;
+            fac= (fac*10) + last;
+            n=n/10;
+        }
+        return fac;
+    }
 }

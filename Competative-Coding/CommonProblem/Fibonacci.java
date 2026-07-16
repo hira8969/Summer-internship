579465a31419a3c722c2e0aec7d1f3161e017563
hira8969 <hk2493200@gmail.com>
2026-07-16 19:10:36 +0530

    solved Fibonacci

--- Competative-Coding/CommonProblem/Fibonacci.java
+++ Competative-Coding/CommonProblem/Fibonacci.java
@@ -1,5 +1,24 @@
 package CommonProblem;
 
+import java.util.Scanner;
+
 public class Fibonacci {
-    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        fibo(n);
+    }
+    public static void fibo(int n)
+    {
+        int first=0;
+        int second=1;
+        int next;
+        for(int i=1;i<=n;i++)
+        {
+            next= first + second;
+            first= second;
+            second= next;
+        }
+        System.out.println("fibonacci number is : "+ next);
+    }
 }
